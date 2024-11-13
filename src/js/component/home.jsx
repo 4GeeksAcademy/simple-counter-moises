79bbb500608ad76f873f4b5f5554b7d0398c07a5
moises2872024 <moises.montt287@gmail.com>
2024-11-13 17:41:28 +0000

    counter finalizado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const SecondsCounter = ({ seconds }) => {
+    const timeString = String(seconds).padStart(6, '0').split('');
 
-//create your first component
+    return (
+        <div className="counter-container">
+            <div className="icon"><i className="fa-regular fa-clock"></i></div>
+            {timeString.map((num, index) => (
+                <div key={index} className="digit">
+                    {num}
+                </div>
+            ))}
+        </div>
+    );
+};
+
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [seconds, setSeconds] = useState(0);
+    const [isCounting, setIsCounting] = useState(false); 
+    const [alertTime, setAlertTime] = useState(null);
+
+    useEffect(() => {
+        let interval = null;
+
+        if (isCounting) {
+            interval = setInterval(() => {
+                setSeconds(prev => prev + 1);
+                if (alertTime !== null && seconds + 1 === alertTime) {
+                    alert(`¡Has alcanzado ${alertTime} segundos!`);
+                }
+            }, 1000);
+        }
+
+        return () => clearInterval(interval);
+    }, [isCounting, seconds, alertTime]);
+
+    const start = () => setIsCounting(true);
+    const stop = () => setIsCounting(false);
+    const reset = () => {
+        setIsCounting(false);
+        setSeconds(0);
+        window.location.reload(); 
+    };
+
+    const handleAlertChange = (e) => {
+        const value = parseInt(e.target.value);
+        if (value >= 0) {
+            setAlertTime(value); 
+        } else {
+            setAlertTime(null); 
+        }
+    };
+
+    return (
+        <div className="main-container">
+            <div className="app-container">
+                <SecondsCounter seconds={seconds} />
+                <div className="button-container">
+                    <button onClick={start} className="modern-button">Iniciar</button>
+                    <button onClick={stop} className="modern-button">Parar</button>
+                    <button onClick={reset} className="modern-button">Reiniciar</button>
+                </div>
+                <div className="alert-input">
+                    <label>
+                        Tiempo limite:
+                        <input 
+                            type="number" 
+                            onChange={handleAlertChange} 
+                            min="0" 
+                        />
+                    </label>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
